a86027d99d0dd3f5ebef7489d83565f0d4bfda3d
maheshpabba <mahesh.pabba@gmail.com>
2021-08-22 12:21:48 -0400

    saf

--- microservices/voting/main.py
+++ microservices/voting/main.py
@@ -90,4 +90,4 @@ def results():
     return str(rv)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',static_url_path='/static',port=5000,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
